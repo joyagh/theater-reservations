e43d9b78507d20c6473bc690fcf77886a4e4cefe
Justine Oyaghiro <justineoyaghiro@icloud.com>
2026-04-14 13:23:17 -0400

    added first souts

--- src/main/java/com/pluralsight/TheaterReservations.java
+++ src/main/java/com/pluralsight/TheaterReservations.java
@@ -7,6 +7,7 @@ import java.util.regex.Pattern;
 
 public class TheaterReservations {
     public static void main(String[] args) {
-        
+        System.out.println("Welcome to Theater Reservations! Please enter your information below to reserve tickets");
+        System.out.print("Full name: ");
     }
 }
